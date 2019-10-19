f223b67878da5f7c1f6f5dbae2e6c8124df6b8a6
liam619 <s3697652@student.rmit.edu.au>
2019-10-19 22:17:39 +1100

    Added db:seed

--- app/models/distributor.rb
+++ app/models/distributor.rb
@@ -3,5 +3,5 @@ class Distributor < ApplicationRecord
   has_and_belongs_to_many :books
 
   VALID_LOCATION = /\A[\d]{2}+\.[\d]{2}+\.[\d]{2}\z/
-  validates :name, presence: true, length:  { minimum:  10 }
+  validates :name, presence: true, length:  { minimum:  5 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Author.new(name: "Administrator RMIT",
+         email: "admin",
+         password: "password",
+         password_confirmation: "password",
+         admin: true).save(validate: false)
+
+Author.create!(name: "William Low",
+             email: "williamlow@rmit.com",
+             password: "@123Qwerty",
+             password_confirmation: "@123Qwerty")
+
+Author.create!(name: "Syed Hariz",
+             email: "syedhariz@rmit.com",
+             password: "Pass123!",
+             password_confirmation: "Pass123!")
+
+drslump = Book.new(name: 'Dr. Slump', author_id: 2, description: 'Dr. Slump is a Japanese manga series written and illustrated by Akira Toriyama.')
+harrypotter = Book.new(name: 'Harry Potter', author_id: 2, description: 'Harry Potter is a series of fantasy novels written by British author J. K. Rowling.')
+wow = Book.new(name: 'World Of Warcraft', author_id: 2, description: 'The World of Warcraft book series by multiple authors includes books Cycle of Hatred, Rise of the Horde, Tides of Darkness, and several more.')
+java = Book.new(name: 'Introduction To Java Programming', author_id: 2, description: 'This is sample Advance Programming for RMIT university')
+
+manga = Category.create!(name: 'Manga')
+fantasy = Category.create!(name: 'Fantasy')
+edu = Category.create!(name: "Education")
+
+drslump.categories << manga
+harrypotter.categories << fantasy
+wow.categories << fantasy
+java.categories << edu
+
+shonen = Distributor.create!(name: 'Weekly Shonen Jump')
+bloom = Distributor.create!(name: 'Bloomsburry')
+pear = Distributor.create!(name: 'Pearson')
+bliz = Distributor.create!(name: 'Blizzard')
+
+drslump.distributors << shonen
+harrypotter.distributors << bloom
+java.distributors << pear
+wow.distributors << bliz
+
+drslump.save(validate: true)
+harrypotter.save(validate: true)
+wow.save(validate: true)
+java.save(validate: true)
